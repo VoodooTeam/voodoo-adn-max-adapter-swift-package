c2a4fe2123ee6dd0406cd1ca0438ca52c7a1fb97
Circle CI <vsauce@voodoo.io>
2024-09-25 20:09:48 +0000

    Release 2.3.45

--- Sources/Constants.swift
+++ Sources/Constants.swift
@@ -5,5 +5,5 @@ enum Constants {
     static let adDisplayErrorCode: Int = -4_205
     static let minimumSdkVersion: Int = 11_020_199
     static let mediationName = "maxads"
-    static let adapterVersion = "2.3.44.1.0"
+    static let adapterVersion = "2.3.45.1.0"
 }
